b78a3fdd45f04a644df00773fb133f1613d22d0c
kirankgouda <107629237+kirankgouda@users.noreply.github.com>
2022-07-22 14:28:22 +0530

    Update Testpro1.java

--- WebDriver/src/test/java/maven/Testpro1.java
+++ WebDriver/src/test/java/maven/Testpro1.java
@@ -48,7 +48,8 @@ public class Testpro1 {
 				driver.findElement(By.id("welcome")).click();
 				Thread.sleep(3000);
 				driver.findElement(By.xpath("//*[@id='welcome-menu']/ul/li[3]/a")).click();
-				System.out.println("hi mrkk");
+				System.out.println("hi mrkk....");
+			  	
 		  }
 
 	  @AfterTest
